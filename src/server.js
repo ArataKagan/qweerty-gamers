969d3b47c7ecd157b2bbb7bbb23b7d93bdb0e4b3
Arata Kagan <aratakagan@gmail.com>
2019-01-22 14:59:07 -0800

    Debugging heroku error

--- src/server.js
+++ src/server.js
@@ -8,16 +8,16 @@ const server = http.createServer(app);
 
 server.listen(port);
 
-function normalizePort(val){
+function normalizePort(val) {
     const port = parseInt(val, 10);
-    if (isNaN(port)){
-        return val;
+    if (isNaN(port)) {
+      return val;
     }
-    if(port >= 0){
-        return port;
+    if (port >= 0) {
+      return port;
     }
     return false;
-}
+  }
 
 
 server.on("listening", () => {
