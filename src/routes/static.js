bff02fcaf28987eabcd46d69863bc855d41847fe
Arata Kagan <aratakagan@gmail.com>
2019-01-21 18:22:53 -0800

    Added staticController

---
+++ src/routes/static.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const staticController = require("../controllers/staticController");
+const router = express.Router();
+
+router.get("/", staticController.index);
+
+module.exports = router;
