f1ef862282b38c086389916ef70d77368c3c6036
Arata Kagan <aratakagan@gmail.com>
2019-01-25 23:42:19 -0800

    Added LGBT news api

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,16 @@
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI('e89e81f136f343719303e1f4d0fdf9c6');
+
 module.exports = {
     index(req, res, next){
-        res.render("static/index");
+       
+       newsapi.v2.everything({
+           q: 'lgbt',
+           language: 'en',
+           page: 2
+       }).then( response => {
+        res.render("static/index", {response});
+        }
+       ) 
     }
 }
